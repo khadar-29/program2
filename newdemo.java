da612bb6980b5eaf9dd7d9b408e6e797f2ec1f1b
khadar-29 <khadarvalimb@gmail.com>
2025-05-26 13:46:08 +0530

    save

--- newdemo.java
+++ newdemo.java
@@ -1,25 +1,30 @@
 public class newdemo {
     private String name;
     private int birthYear;
+    private String usn; // Added attribute
 
-    newdemo(String name, int birthYear) {
+    // Updated constructor to accept and set 'usn'
+    newdemo(String name, int birthYear, String usn) {
         this.name = name;
         this.birthYear = birthYear;
+        this.usn = usn;
     }
 
     public void displayStudentInfo() {
         int currentYear = 2025; // Hardcoded current year
         int age = currentYear - birthYear;
         System.out.println("Student Name: " + name);
+        System.out.println("USN: " + usn);
         System.out.println("Age: " + age);
     }
 
     public static void main(String[] args) {
-        // Hardcoded input instead of scanner
-        String name = "John Doe";
+        
+        String name = "Khadar";
         int birthYear = 2000;
+        String usn = "01fe23bca160";
 
-        newdemo student = new newdemo(name, birthYear);
+        newdemo student = new newdemo(name, birthYear, usn);
         student.displayStudentInfo();
     }
 }
